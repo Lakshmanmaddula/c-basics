2c56bede7a61b6284d01e57dcdcc1c46b88b6158
Lakshmanmaddula <92910403+Lakshmanmaddula@users.noreply.github.com>
2021-10-22 12:20:56 +0530

    Add files via upload

---
+++ My Friends.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+	 printf("lakshman");
+	 printf("\n20P35A03D1");
+	 printf("\nMechanical Engineering");
+	 printf("\nB-Tech");
+	 printf("\nChaitanya Raju");
+	 printf("\n20P35A03B2");
+	 printf("\nMechanical Engineering");
+	 printf("\nB-Tech");
+	 printf("\nvinay");
+	 printf("\n20P35A03D4");
+	 printf("\nMechanical Engineering");
+	 printf("\nB-Tech");
+	 printf("\nshiva manikanta");
+	 printf("\n20P35A03A4");
+	 printf("\nMechanical Engineering");
+	 printf("\nB-Tech");
+}
